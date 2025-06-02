a83d58cadcb6760694ff2948508150e3c3e6b0fe
Berdyshev Kerey <berdyshev_k1004@akb.nis.edu.kz>
2025-06-02 16:39:33 +0500

    initial structure

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { MapPin, Clock, Users, Award } from "lucide-react"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { ArrowRightIcon, CalendarIcon } from "@radix-ui/react-icons"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <h1>Jolaushy - smart AI trip planner</h1>
+    </main>
+  )
+}
